449b9ff39100d0421742266b088878b9fff48a91
Andrei Negreț <andrei.negret@yahoo.com>
2021-03-28 18:07:28 +0300

    Test commit

--- Program.cs
+++ Program.cs
@@ -1,11 +1,10 @@
+using IronPython.Hosting;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text;
 
 namespace MusicBud
 {
---
+++ wwwroot/js/Modules/webCamFeed.js
@@ -0,0 +1,30 @@
+﻿const video = document.getElementById('video')
+
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('../Models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('../Models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('../Models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('../Models')
+]).then(startVideo)
+
+function startVideo() {
+    navigator.getUserMedia(
+        { video: {} },
+        stream => video.srcObject = stream,  
+        err => console.error(err)
+    )
+}
+
+startvideo()
+
+document.getElementById('snap').addEventListener('click', () => {
+    context.drawImage(video, 0, 0, 640, 480);
+})
+
+video.addEventListener('play', () => {
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video,
+            new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+        console.log(detections)
+    }, 100)
+})
