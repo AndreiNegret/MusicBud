ca213c28d3075ff23b0d1598c392564d79c23091
Andrei Negreț <andrei.negret@yahoo.com>
2021-04-04 21:23:17 +0300

    Updated user interface and camera bug fixes

--- wwwroot/js/Modules/webCamFeed.js
+++ wwwroot/js/Modules/webCamFeed.js
@@ -1,30 +1,46 @@
-﻿const video = document.getElementById('video')
+﻿const vid = document.querySelector('video');
+navigator.mediaDevices.getUserMedia({ video: true }) // request cam
+    .then(stream => {
+        vid.srcObject = stream; // don't use createObjectURL(MediaStream)
+        return vid.play(); // returns a Promise
+    })
+    .then(() => { // enable the button
+        const btn = document.querySelector('button');
+        btn.disabled = false;
+        btn.onclick = e => {
+            takeASnap()
+                .then(sendToServer);
+        };
+    });
 
-Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri('../Models'),
-    faceapi.nets.faceLandmark68Net.loadFromUri('../Models'),
-    faceapi.nets.faceRecognitionNet.loadFromUri('../Models'),
-    faceapi.nets.faceExpressionNet.loadFromUri('../Models')
-]).then(startVideo)
-
-function startVideo() {
-    navigator.getUserMedia(
-        { video: {} },
-        stream => video.srcObject = stream,  
-        err => console.error(err)
-    )
+function takeASnap() {
+    const canvas = document.createElement('canvas'); // create a canvas
+    const ctx = canvas.getContext('2d'); // get its context
+    canvas.width = vid.videoWidth; // set its size to the one of the video
+    canvas.height = vid.videoHeight;
+    ctx.drawImage(vid, 0, 0); // the video
+    return new Promise((res, rej) => {
+        canvas.toBlob(res, 'image/jpeg'); // request a Blob from the canvas
+    });
 }
 
-startvideo()
-
-document.getElementById('snap').addEventListener('click', () => {
-    context.drawImage(video, 0, 0, 640, 480);
-})
-
-video.addEventListener('play', () => {
-    setInterval(async () => {
-        const detections = await faceapi.detectAllFaces(video,
-            new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-        console.log(detections)
-    }, 100)
-})
+function sendToServer(blob) {
+    // uses the <a download> to download a Blob
+    const reader = new FileReader();
+    reader.readAsDataURL(blob);
+    reader.onloadend = function () {
+        const base64data = reader.result;
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost:1337',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                imgData: base64data,
+                imgWidth: vid.videoWidth,
+                imgHeight: vid.videoHeight
+            }),
+            processData: false,
+            dataType: 'json'
+        });
+    }
+}
